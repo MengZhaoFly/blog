cfb57993093f8147afa3c0563e00648ec2e3d165
MengZhaoFly <1424254461@qq.com>
2020-06-29 15:20:05 +0800

    update

--- js-base/es7-es11/es8/trailing-commas.js
+++ js-base/es7-es11/es8/trailing-commas.js
@@ -11,10 +11,11 @@ const trailingArray = ['value1',
   'value2',
   'value3',
 ];
-
+// diff
 // Object Declaration
 const trailingObject = {
   key1: 'value1',
   key2: 'value2',
   key3: 'value3',
+  key4: 'cccccc'
 };
